e24cac4af18f4413c61237dd9136465d24a8ab0c
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-04-09 15:46:06 -0300

    fix:contador in idividual component

--- 01-Bases/src/app/contador/contador.component.ts
+++ 01-Bases/src/app/contador/contador.component.ts
@@ -2,14 +2,19 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-contador',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./contador.component.css'],
   template: `
+    <h1>{{ title }}</h1>
+    <hr>
+    <h3>La base es <strong>{{ base }}</strong></h3>
+
     <button (click)="acumular(base);">{{ "+" + base }}</button>
     <span>{{ numero }}</span>
     <button (click)="acumular(-base)">{{ "-" + base }}</button>
   `
 })
 export class ContadorComponent{
+  public title  : string = 'Contador APP';
   public numero : number = 10;
   public base   : number = 5;
 
