48c75ce183939e4028dde1b5730c3b55bcade6fc
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-04-11 12:14:46 -0300

    feat:59. ngModel

--- 01-Bases/src/app/dbz/main-page/main-page.component.ts
+++ 01-Bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 
+interface Personaje{
+  nombre: string;
+  poder: number;
+};
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent{
+  public nuevo: Personaje = {
+    nombre: "Trucks",
+    poder: 14000
+  };
+
   public agregar(): void{
     console.log("test");
+    console.log(this.nuevo);
   };
 }
