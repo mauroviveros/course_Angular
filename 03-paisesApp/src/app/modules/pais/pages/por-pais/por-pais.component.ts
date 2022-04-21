67fbc32a8e4efb55cf5e473a7f088c794511b1ed
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-04-20 23:12:41 -0300

    feat:104. Componente para buscar por país

--- 03-paisesApp/src/app/modules/pais/pages/por-pais/por-pais.component.ts
+++ 03-paisesApp/src/app/modules/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-por-pais',
@@ -6,11 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent{
+  public termino: string = "";
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  buscar(): void{
+    console.log(this.termino);
+  };
 }
