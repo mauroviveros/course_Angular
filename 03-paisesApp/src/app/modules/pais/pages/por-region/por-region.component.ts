fcd04a536f3fa1744555d3ce84c0e3ed206ee8dd
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-04-22 10:41:21 -0300

    feat:124. Mostrar países por región

--- 03-paisesApp/src/app/modules/pais/pages/por-region/por-region.component.ts
+++ 03-paisesApp/src/app/modules/pais/pages/por-region/por-region.component.ts
@@ -1,18 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html'
 })
-export class PorRegionComponent {
-  public regiones     : string[]  = ["africa", "americas", "asia", "europe", "oceania"];
+export class PorRegionComponent implements OnInit {
+  public regiones     : string[]  = ["africa", "america", "asia", "europe", "oceania"];
   public regionActiva : string    = "";
+  public paises       : Country[] = [];
+
+  constructor(
+    private paisService: PaisService
+  ){};
+
+  ngOnInit(){
+    this.activarRegion(this.regiones[0]);
+  }
 
   public getClass(region: string): string{
     return region == this.regionActiva ? "active" : "";
   };
 
   public activarRegion(region: string): void{
+    if(region === this.regionActiva) return;
+
     this.regionActiva = region;
+
+    this.paisService.buscarRegion(region).subscribe({
+      next: (resp): void =>{ this.paises = resp; },
+    });
   };
 }
