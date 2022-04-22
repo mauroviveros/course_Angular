8228b7dc74978775bb6a7add168401b92d0c65d2
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-04-22 13:35:24 -0300

    feat:134. Introducción a los Pipes de Angular

--- 04-pipesApp/src/app/app.component.ts
+++ 04-pipesApp/src/app/app.component.ts
@@ -5,5 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'pipesApp';
+  public nombre: string = "mAuRo";
+  public numero: number = 1000;
+  public obj = {
+    nombre: "Mauro"
+  }
+
+  public mostrarNombre(){
+    console.table({
+      nombre: this.nombre,
+      numero: this.numero,
+      obj: this.obj
+    });
+  }
 }
