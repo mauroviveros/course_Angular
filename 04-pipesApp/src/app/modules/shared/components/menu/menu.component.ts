25e46ab17a578d77fd26b6d6cd7ea5fcba91a652
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-04-24 19:03:09 -0300

    feat:141. Cambiar las rutas utilizando el MenuBar

--- 04-pipesApp/src/app/modules/shared/components/menu/menu.component.ts
+++ 04-pipesApp/src/app/modules/shared/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLinkActive } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 
 @Component({
@@ -7,36 +8,39 @@ import { MenuItem } from 'primeng/api';
   styles: []
 })
 export class MenuComponent implements OnInit {
-
+  public icon: string;
   public items: MenuItem[];
 
   constructor(){
+    this.icon = "./favicon.ico";
     this.items = [
       {
-        label: 'File',
+        label: "Pipes de Angular",
+        icon: "pi pi-desktop",
         items: [
           {
-            label: 'New',
-            icon: 'pi pi-fw pi-plus',
-            items: [
-              { label: 'Project' },
-              { label: 'Other' }
-            ]
+            label: "Textos y Fechas",
+            icon: "pi pi-align-left",
+            routerLink: "/",
           },
-          { label: 'Open' },
-          { label: 'Quit' }
+          {
+            label: "Números",
+            icon: "pi pi-dollar",
+            routerLink: "/numeros"
+          },
+          {
+            label: "No comunes",
+            icon: "pi pi-globe",
+            routerLink: "/no-comunes"
+          },
         ]
       },
       {
-        label: 'Edit',
-        icon: 'pi pi-fw pi-pencil',
-        items: [
-          { label: 'Delete', icon: 'pi pi-fw pi-trash' },
-          { label: 'Refresh', icon: 'pi pi-fw pi-refresh' }
-        ]
+        label: "Pipes personalizados",
+        icon: "pi pi-cog"
       }
-    ]
-  }
+    ];
+  };
 
   ngOnInit(): void {}
 }
