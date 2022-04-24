e5a9140a3fce3547d5154017fa1786c47c227ac3
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-04-24 15:01:37 -0300

    feat:138. PrimeNg module

--- 04-pipesApp/src/app/app.module.ts
+++ 04-pipesApp/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app.routing';
+import { PrimeNgModule } from './modules/prime-ng/prime-ng.module';
+
 import { AppComponent } from './app.component';
 
-import {ButtonModule} from 'primeng/button';
-import { CardModule } from "primeng/card";
+
 
 @NgModule({
   declarations: [
@@ -14,8 +15,7 @@ import { CardModule } from "primeng/card";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ButtonModule,
-    CardModule
+    PrimeNgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
