ef4893577deb243536530d9368934a75b0198ce8
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-04-27 19:22:01 -0300

    feat:143. UpperCase, LowerCase y TitleCase Pipes

--- 04-pipesApp/src/app/modules/ventas/pages/basicos/basicos.component.ts
+++ 04-pipesApp/src/app/modules/ventas/pages/basicos/basicos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-basicos',
@@ -6,11 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class BasicosComponent {
+  public nombreLower    : string = "mauro";
+  public nombreUpper    : string = "MAURO";
+  public nombreCompleto : string = "mAuRo ViVeRoS";
 }
