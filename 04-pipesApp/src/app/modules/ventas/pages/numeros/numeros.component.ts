7fdc140ee80cc61f335934caf0bcd6715ab5d47f
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-05-27 20:26:30 -0300

    feat:148. CurrencyPipe y PercentPipe

--- 04-pipesApp/src/app/modules/ventas/pages/numeros/numeros.component.ts
+++ 04-pipesApp/src/app/modules/ventas/pages/numeros/numeros.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numeros',
@@ -6,11 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NumerosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class NumerosComponent{
+  public ventasNetas: number = 2567789.5567;
+  public porcentaje : number = 0.48;
+  a: number = 0.259;
+  b: number = 1.3495;
 }
