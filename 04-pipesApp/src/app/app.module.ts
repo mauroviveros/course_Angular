e1ec1ed1df1f80c9b5466566872acda82bbbc5a0
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-05-27 20:49:57 -0300

    feat:149. PrimeNg - Fieldset

--- 04-pipesApp/src/app/app.module.ts
+++ 04-pipesApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app.routing';
 import { SharedModule } from './modules/shared/shared.module';
@@ -20,6 +21,7 @@ registerLocaleData(localeFR);
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     SharedModule,
     VentasModule
