1fad0bc972130a939bb8013d42b1a7cfd34d8d35
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-09-16 12:33:10 -0300

    feat:168. Primeng - Sortable Table

--- 04-pipesApp/src/app/modules/ventas/pipes/ordenar.pipe.ts
+++ 04-pipesApp/src/app/modules/ventas/pipes/ordenar.pipe.ts
@@ -7,7 +7,7 @@ import { Heroe } from '../interfaces/ventas.interface';
 export class OrdenarPipe implements PipeTransform {
   transform(heroes: Heroe[], orderBy: keyof Heroe | null = null): Heroe[]{
     let values: number[] = [1, -1];
-    if(orderBy != "nombre") values = [-1, 1];
+    if(orderBy == "vuela") values = [-1, 1];
 
     if(orderBy) return heroes.sort((a, b) => (a[orderBy] > b[orderBy]) ? values[0] : values[1]);
     else return heroes;
