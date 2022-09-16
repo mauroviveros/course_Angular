bff04aeea5e0d1037736d36cc0bf69692a2c4308
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-09-15 22:21:56 -0300

    feat:161. Pipe personalizado - mayusculasPipe

--- 04-pipesApp/src/app/modules/ventas/ventas.module.ts
+++ 04-pipesApp/src/app/modules/ventas/ventas.module.ts
@@ -7,6 +7,8 @@ import { NoComunesComponent } from './pages/no-comunes/no-comunes.component';
 import { BasicosComponent } from './pages/basicos/basicos.component';
 import { OrdenarComponent } from './pages/ordenar/ordenar.component';
 
+import { MayusculasPipe } from './pipes/mayusculas.pipe';
+
 
 
 @NgModule({
@@ -14,7 +16,8 @@ import { OrdenarComponent } from './pages/ordenar/ordenar.component';
     NumerosComponent,
     NoComunesComponent,
     BasicosComponent,
-    OrdenarComponent
+    OrdenarComponent,
+    MayusculasPipe
   ],
   exports: [
     NumerosComponent,
