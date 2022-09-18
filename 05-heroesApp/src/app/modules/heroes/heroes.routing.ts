e3a0c675e156ff087a13e102d4f00961d4b65310
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-09-18 15:33:37 -0300

    feat:179. Mostrar Rutas Hijas - Segundo RouterOutlet

--- 05-heroesApp/src/app/modules/heroes/heroes.routing.ts
+++ 05-heroesApp/src/app/modules/heroes/heroes.routing.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 
 
 const routes: Routes = [
   {
     path: "",
+    component: HomeComponent,
     children: [
       { path: "listado",    component: ListadoComponent },
       { path: "agregar",    component: AgregarComponent },
