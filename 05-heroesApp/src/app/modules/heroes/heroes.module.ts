58cb0b3c9cc5b776ff1199d826a6c61191b20e08
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-09-18 15:44:26 -0300

    feat:184. Continuación del proyecto - HeroesApp

--- 05-heroesApp/src/app/modules/heroes/heroes.module.ts
+++ 05-heroesApp/src/app/modules/heroes/heroes.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { HeroesRoutingModule } from './heroes.routing';
 
 import { AgregarComponent } from './pages/agregar/agregar.component';
@@ -21,6 +22,7 @@ import { ListadoComponent } from './pages/listado/listado.component';
   ],
   imports: [
     CommonModule,
+    FlexLayoutModule,
     HeroesRoutingModule
   ]
 })
