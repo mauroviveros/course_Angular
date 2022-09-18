6608f902a7cfd65b05cd9961c63ab7f394d03196
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-09-18 14:56:00 -0300

    feat:176. Rutas hijas y LazyLoad - AuthRoutes

--- 05-heroesApp/src/app/app.routing.ts
+++ 05-heroesApp/src/app/app.routing.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { ErrorPageComponent } from './shared/components/error-page/error-page.component';
 
 const routes: Routes = [
+  {
+    path: "auth",
+    loadChildren: () => import("./modules/auth/auth.module").then(m => m.AuthModule)
+  },
   {
     path: "404",
     component: ErrorPageComponent
@@ -11,7 +16,7 @@ const routes: Routes = [
     path: "**",
     redirectTo: "404"
   }
-]
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
