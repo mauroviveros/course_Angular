cc6a1a98876c5d39e28c258e0cb6829dc8e66460
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-09-18 16:30:37 -0300

    feat:186. Material Navlist

--- 05-heroesApp/src/app/modules/material/material.module.ts
+++ 05-heroesApp/src/app/modules/material/material.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 @NgModule({
   exports: [
-    MatSidenavModule,
-    MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule { }
