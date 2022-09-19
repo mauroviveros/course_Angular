f3c6b4f98cd14f64e69983bdd5307f5da9189470
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-09-19 14:20:42 -0300

    feat:192. Tarea - HeroeTarjetaComponent

---
+++ 05-heroesApp/src/app/modules/heroes/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { Heroe } from '../../interfaces/heroe.interface';
+
+@Component({
+  selector: 'app-heroe-tarjeta',
+  templateUrl: './heroe-tarjeta.component.html',
+  styleUrls: ['./heroe-tarjeta.component.scss']
+})
+export class HeroeTarjetaComponent{
+  @Input() public heroe!: Heroe;
+}
