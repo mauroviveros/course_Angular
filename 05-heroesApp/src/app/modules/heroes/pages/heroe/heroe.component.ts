c3e6c847b6a6650f1dda8f03314a82c2b8957ff1
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-09-19 14:50:20 -0300

    feat:194. Tarea - Ruta Héroe y Editar Héroe

--- 05-heroesApp/src/app/modules/heroes/pages/heroe/heroe.component.ts
+++ 05-heroesApp/src/app/modules/heroes/pages/heroe/heroe.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Heroe } from '../../interfaces/heroe.interface';
 
 @Component({
   selector: 'app-heroe',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroe.component.scss']
 })
 export class HeroeComponent implements OnInit {
+  public heroe: Heroe | null = null;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute){
+
+  };
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(({ id }) => console.log(id));
   }
 
 }
