5ce971adbe2320040d8b4f113fb4f8a183be570a
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-09-22 01:39:04 -0300

    feat:226. Mantener la sesión del usuario

--- 05-heroesApp/src/app/modules/auth/guards/auth.guard.ts
+++ 05-heroesApp/src/app/modules/auth/guards/auth.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanLoad, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, CanLoad, Router, UrlTree } from '@angular/router';
+import { Observable, map } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
@@ -10,15 +10,13 @@ export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(private router: Router, private authService: AuthService) { };
 
-  canActivate(): boolean | UrlTree {
-    const isActive: boolean = !!this.authService.userActive.id;
-    if (isActive) return true;
-    else return this.router.parseUrl("/");
+  canActivate(): Observable<boolean | UrlTree> {
+    return this.authService.isAuthenticated()
+      .pipe(map(isAuth => isAuth ? isAuth : this.router.parseUrl("/login")));
   };
 
-  canLoad(): boolean | UrlTree {
-    const isActive: boolean = !!this.authService.userActive.id;
-    if (isActive) return true;
-    else return this.router.parseUrl("/");
+  canLoad(): Observable<boolean | UrlTree> {
+    return this.authService.isAuthenticated()
+      .pipe(map(isAuth => isAuth ? isAuth : this.router.parseUrl("/login")));
   };
 };
--- 05-heroesApp/src/app/modules/auth/services/auth.service.ts
+++ 05-heroesApp/src/app/modules/auth/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap } from "rxjs";
+import { Observable, of, tap, map } from "rxjs";
 import { environment } from 'src/environments/environment';
 
 import { User } from '../interfaces/auth.interface';
@@ -12,17 +12,26 @@ export class AuthService {
   private _endPoint: string = environment.endPoint;
   private _user: User = {} as User;
 
-  constructor(private http: HttpClient) { };
+  constructor(private http: HttpClient) {
+  };
 
   get userActive(){
     return { ...this._user }
   };
 
+  isAuthenticated(): Observable<boolean>{
+    if(!localStorage.getItem("token")) return of(false);
+    return this.login().pipe(map(() => true));
+  };
+
   login(){
-    return this.http.get<User>(`${this._endPoint}/usuarios/1`).pipe(tap(user => this._user = user));
+    return this.http.get<User>(`${this._endPoint}/usuarios/1`)
+      .pipe(tap(user => this._user = user))
+      .pipe(tap(({ id }) => localStorage.setItem("token", id.toString())))
   };
 
   logout(){
+    localStorage.removeItem("token");
     this._user = {} as User;
   };
 
