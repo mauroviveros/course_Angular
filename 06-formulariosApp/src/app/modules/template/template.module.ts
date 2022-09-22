c45a0d5c46081cbc06269ed24b377607c1a07e7a
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-09-22 18:31:42 -0300

    feat:231. Creación de módulos necesarios

---
+++ 06-formulariosApp/src/app/modules/template/template.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TemplateRoutingModule } from './template.routing';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TemplateRoutingModule
+  ]
+})
+export class TemplateModule { };
