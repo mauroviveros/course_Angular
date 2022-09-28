7b1db6713572f2d6990db4c113a5ff37817c8533
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-09-28 13:22:04 -0300

    feat:233. SideMenu

--- 06-formulariosApp/src/app/shared/components/sidemenu/sidemenu.component.ts
+++ 06-formulariosApp/src/app/shared/components/sidemenu/sidemenu.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+interface MenuItem{
+  text  : string;
+  route : string;
+};
+
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
   styleUrls: ['./sidemenu.component.scss']
 })
 export class SidemenuComponent{
-  constructor() { };
+  public templateMenu: MenuItem[] = [
+    { text: "Basicos", route: "./template/basicos" },
+    { text: "Dinámicos", route: "./template/dinamicos" },
+    { text: "Switches", route: "./template/switches" }
+  ];
+
+  public reactiveMenu: MenuItem[] = [
+    { text: "Basicos", route: "./reactive/basicos" },
+    { text: "Dinámicos", route: "./reactive/dinamicos" },
+    { text: "Switches", route: "./reactive/switches" }
+  ];
 };
--- 06-formulariosApp/src/app/shared/shared.module.ts
+++ 06-formulariosApp/src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidemenuComponent } from './components/sidemenu/sidemenu.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -9,7 +10,8 @@ import { SidemenuComponent } from './components/sidemenu/sidemenu.component';
     SidemenuComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     SidemenuComponent
