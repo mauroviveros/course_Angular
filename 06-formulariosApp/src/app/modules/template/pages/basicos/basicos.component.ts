e7928e3e665ae883b5f1a4c4c2a601a07421719d
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-09-28 21:02:30 -0300

    feat:235. Template: FormsModule

--- 06-formulariosApp/src/app/modules/template/pages/basicos/basicos.component.ts
+++ 06-formulariosApp/src/app/modules/template/pages/basicos/basicos.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
   styleUrls: ['./basicos.component.scss']
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent{
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  public guardarForm(form: NgForm){
+    console.log(form.value);
+  };
 }
--- 06-formulariosApp/src/app/modules/template/template.module.ts
+++ 06-formulariosApp/src/app/modules/template/template.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { TemplateRoutingModule } from './template.routing';
 import { SwitchesComponent } from './pages/switches/switches.component';
@@ -15,6 +16,7 @@ import { BasicosComponent } from './pages/basicos/basicos.component';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     TemplateRoutingModule
   ]
 })
