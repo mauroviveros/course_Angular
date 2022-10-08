6c920c215d5d1f734fcb32e72f2c3ff61d9b2e57
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-10-08 19:49:24 -0300

    feat:246. Template: Validando Radios, Checks y Switches

--- 06-formulariosApp/src/app/modules/template/pages/switches/switches.component.ts
+++ 06-formulariosApp/src/app/modules/template/pages/switches/switches.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 
+enum Genero{
+  femenino = "F",
+  masculino = "M"
+};
+interface Persona{
+  genero: Genero,
+  notificaciones: boolean,
+  terminos: boolean
+};
+
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
   styleUrls: ['./switches.component.scss']
 })
 export class SwitchesComponent {
+
+  public persona = {} as Persona;
 
   constructor() { }
 
