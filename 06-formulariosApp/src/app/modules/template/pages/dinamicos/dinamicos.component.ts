7531bc29bad43270fd62fc39de549292bf19c062
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-10-08 18:59:23 -0300

    feat:242. Agregar elementos de forma dinámica

--- 06-formulariosApp/src/app/modules/template/pages/dinamicos/dinamicos.component.ts
+++ 06-formulariosApp/src/app/modules/template/pages/dinamicos/dinamicos.component.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component } from '@angular/core';
+
+interface Persona{
+  nombre: string,
+  favoritos: Favorito[]
+};
+
+interface Favorito{
+  id: number,
+  nombre: string
+}
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styleUrls: ['./dinamicos.component.scss']
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent{
+
+  public persona: Persona = {
+    nombre: "Mauro",
+    favoritos: [
+      { id: 1, nombre: "Metal Gear" },
+      { id: 2, nombre: "DeathStranding" }
+    ]
+  };
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
   public guardar(form: any): void{
     console.log("Form POST");
