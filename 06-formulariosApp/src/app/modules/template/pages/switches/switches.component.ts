ff6482d27dcd52ae5b9d1ccf396639b9f1c8087b
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-10-08 19:33:28 -0300

    feat:245. Template: Radio, Check y Switches

--- 06-formulariosApp/src/app/modules/template/pages/switches/switches.component.ts
+++ 06-formulariosApp/src/app/modules/template/pages/switches/switches.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
   styleUrls: ['./switches.component.scss']
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  public guardar(form: any): void{
+    console.log("Form POST");
+    console.log(form);
+  };
 }
