08f72204d9535f6bcd3dddf3f61fd898b017927f
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-10-10 22:32:32 -0300

    feat:257. FormArray

--- 06-formulariosApp/src/app/modules/reactive/pages/dinamicos/dinamicos.component.ts
+++ 06-formulariosApp/src/app/modules/reactive/pages/dinamicos/dinamicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -9,12 +9,21 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class DinamicosComponent {
 
   public form: FormGroup = this.formBuilder.group({
-    nombre: [ null, [ Validators.required, Validators.minLength(3) ] ]
+    nombre    : [ null, [ Validators.required, Validators.minLength(3) ] ],
+    favoritos : this.formBuilder.array([
+      [ "Metal Gear", Validators.required ],
+      [ "Death Stranding", Validators.required ]
+    ], Validators.required)
   });
+
+  get favoritos(): AbstractControl[] {
+    return (this.form.get('favoritos') as FormArray).controls;
+  };
 
   constructor(
     private formBuilder: FormBuilder
   ) { };
+
 
   public campoNoEsValido(campo: string): boolean | null{
     return this.form.controls[campo].errors && this.form.controls[campo].touched;
