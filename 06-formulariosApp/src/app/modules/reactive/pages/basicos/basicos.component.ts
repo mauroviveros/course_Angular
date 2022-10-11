cd2c961797282f0b2b302bab76ca65311e13122b
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-10-10 21:39:10 -0300

    feat:253. Validaciones básicas - Forms Validator

--- 06-formulariosApp/src/app/modules/reactive/pages/basicos/basicos.component.ts
+++ 06-formulariosApp/src/app/modules/reactive/pages/basicos/basicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -9,9 +9,9 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 export class BasicosComponent{
 
   public form: FormGroup = this.formBuilder.group({
-    producto    : ['RTX 4080ti'],
-    precio      : [1500],
-    existencias : [5]
+    producto    : [ 'RTX 4080ti', [ Validators.required, Validators.minLength(3) ] ],
+    precio      : [ 1500, [ Validators.required, Validators.min(0) ] ],
+    existencias : [ 5, [ Validators.required, Validators.min(0) ] ]
   })
 
   constructor(
