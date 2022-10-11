24b8bc008b0c64f3dd1de143343704f57092db96
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-10-11 19:32:42 -0300

    feat:269. Validar contra una expresión regular

--- 06-formulariosApp/src/app/modules/auth/auth.module.ts
+++ 06-formulariosApp/src/app/modules/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth.routing';
 import { LoginComponent } from './pages/login/login.component';
@@ -13,6 +14,7 @@ import { RegisterComponent } from './pages/register/register.component';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     AuthRoutingModule
   ]
 })
--- 06-formulariosApp/src/app/modules/auth/pages/register/register.component.ts
+++ 06-formulariosApp/src/app/modules/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  public regexp_fullname: string = "([a-zA-Z]+) ([a-zA-Z]+)";
 
-  constructor() { }
+  public form: FormGroup = this.formBuilder.group({
+    name: [null, [Validators.required, Validators.pattern(this.regexp_fullname)]],
+    email: null,
+    username: null,
+    password: null,
+    password_confirm: null
+  });
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { };
 
   ngOnInit(): void {
-  }
+  };
 
-}
+  public hasErrors(field: string, validator: string): boolean {
+    if(!this.form.controls[field].touched) return false;
+    return this.form.controls[field].errors?.[validator];
+  };
+
+  public guardar(): void{
+    this.form.markAllAsTouched();
+    if(this.form.invalid) return;
+
+    console.log(this.form.value);
+  };
+};
