f77ba068a02a7ab91a81550ad94dacd2787de184
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-10-10 23:48:49 -0300

    feat:261. Actualizar el valor de la persona

--- 06-formulariosApp/src/app/modules/reactive/pages/switches/switches.component.ts
+++ 06-formulariosApp/src/app/modules/reactive/pages/switches/switches.component.ts
@@ -1,28 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
   styleUrls: ['./switches.component.scss']
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent {
   public form: FormGroup = this.formBuilder.group({
-    genero          : [ null, Validators.required ],
-    notificaciones  : [ null, Validators.required ],
-    terminos        : [ null, Validators.requiredTrue ]
+    terminos  : [ null, Validators.requiredTrue ],
+    persona   : this.formBuilder.group({
+      genero          : [ null, Validators.required ],
+      notificaciones  : [ null ]
+    })
   });
-
-  public persona = {
-    notificaciones: false,
-  };
 
   constructor(
     private formBuilder: FormBuilder
   ) { };
 
-  ngOnInit(): void {
-    this.form.reset(this.persona);
+  guardar(): void {
+    this.form.markAllAsTouched();
+    if(this.form.invalid) return;
+
+    console.log((this.form.get('persona') as FormArray).value);
+    console.log((this.form.get('terminos') as FormArray).value);
+
+    this.form.reset();
   };
 
 };
