060ac3ada0541fe078e7e9f0672d547452d9e725
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-11-25 10:28:19 -0300

    feat:287. Estructura de directorios para esta aplicación

--- 07-selectores/src/app/app.routing.ts
+++ 07-selectores/src/app/app.routing.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () => import("./modules/paises/paises.module").then(m => m.PaisesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
