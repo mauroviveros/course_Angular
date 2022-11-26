f93758cb838794288e435cd81f73e3e75810cb79
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-11-26 14:08:52 -0300

    feat:289. Selector de regiones

--- 07-selectores/src/app/modules/paises/pages/selector-page/selector-page.component.ts
+++ 07-selectores/src/app/modules/paises/pages/selector-page/selector-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { PaisesService } from '../../services/paises.service';
+
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
@@ -8,16 +10,18 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class SelectorPageComponent implements OnInit {
 
+  public regiones: string[] = [];
   public form: FormGroup = this.formBuilder.group({
     region: ['', Validators.required]
   });
 
-
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private paisesService: PaisesService
   ) { }
 
   ngOnInit(): void {
+    this.regiones = this.paisesService.regiones;
   }
 
   public guardar(){
--- 07-selectores/src/app/modules/paises/services/paises.service.ts
+++ 07-selectores/src/app/modules/paises/services/paises.service.ts
@@ -5,5 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class PaisesService {
 
+  public regiones: string[] = ["Africa", "Americas", "Asia", "Europe", "Oceania"];
+
   constructor() { }
 }
