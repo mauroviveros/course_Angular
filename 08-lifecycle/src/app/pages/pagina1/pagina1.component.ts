fcd180f419b0da9b9ffba5740e5f5a669f49ad08
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-11-28 19:55:59 -0300

    feat:299. Inicio de proyecto - LifeCycle

---
+++ 08-lifecycle/src/app/pages/pagina1/pagina1.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina1',
+  templateUrl: './pagina1.component.html',
+  styleUrls: ['./pagina1.component.scss']
+})
+export class Pagina1Component {};
