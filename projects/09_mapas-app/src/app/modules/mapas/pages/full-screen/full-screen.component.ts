773e4d01aa5d4085c74711dc541bc58bc9b06e62
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-12-08 11:27:07 -0300

    feat:312. Mostrar un mapa en pantalla completa

--- projects/09_mapas-app/src/app/modules/mapas/pages/full-screen/full-screen.component.ts
+++ projects/09_mapas-app/src/app/modules/mapas/pages/full-screen/full-screen.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../../../environments/environment';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-full-screen',
-  template: `<p>full-screen works!</p>`,
-  styles: []
+  template: `<div id="mapa"></div>`,
+  styleUrls: ['./full-screen.component.scss']
 })
 export class FullScreenComponent {
+  constructor(){};
 
+  ngOnInit(): void{
+    (mapboxgl as any).accessToken = environment.MAPBOX_TOKEN;
+
+    const map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11'
+    })
+  };
 }
