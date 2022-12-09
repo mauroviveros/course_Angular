1340725495a7df5bf77e59fbb579626f00235d89
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-12-09 11:59:53 -0300

    fix:Uso ElementRef en fullScreen

--- projects/09_mapas-app/src/app/modules/mapas/pages/full-screen/full-screen.component.ts
+++ projects/09_mapas-app/src/app/modules/mapas/pages/full-screen/full-screen.component.ts
@@ -1,18 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-full-screen',
-  template: `<div class="mapa-wrapper"></div>`,
+  template: `<div class="mapa-wrapper" #mapa></div>`,
   styles: []
 })
 export class FullScreenComponent {
-  constructor(){};
+  @ViewChild('mapa') public mapaWrapper: ElementRef = {} as ElementRef;
 
-  ngOnInit(): void{
-    const map = new mapboxgl.Map({
-      container: 'mapa',
+
+  ngAfterViewInit(): void{
+    new mapboxgl.Map({
+      container: this.mapaWrapper.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [ -58.444216881596624, -34.60446199121021 ],
       zoom: 18
