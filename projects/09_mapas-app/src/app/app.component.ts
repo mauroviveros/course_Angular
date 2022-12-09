e22d46b5a8b6408f8b115691b97719c3b21dfc6e
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-12-09 10:30:28 -0300

    feat:313. Punto central, zoom y accessToken de forma global

--- projects/09_mapas-app/src/app/app.component.ts
+++ projects/09_mapas-app/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from '../environments/environment';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  
+  ngOnInit(): void{
+    (mapboxgl as any).accessToken = environment.MAPBOX_TOKEN;
+  };
 }
--- projects/09_mapas-app/src/app/modules/mapas/pages/full-screen/full-screen.component.ts
+++ projects/09_mapas-app/src/app/modules/mapas/pages/full-screen/full-screen.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { environment } from '../../../../../environments/environment';
+
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -11,11 +11,11 @@ export class FullScreenComponent {
   constructor(){};
 
   ngOnInit(): void{
-    (mapboxgl as any).accessToken = environment.MAPBOX_TOKEN;
-
     const map = new mapboxgl.Map({
       container: 'mapa',
-      style: 'mapbox://styles/mapbox/streets-v11'
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [ -58.444216881596624, -34.60446199121021 ],
+      zoom: 18
     })
   };
 }
