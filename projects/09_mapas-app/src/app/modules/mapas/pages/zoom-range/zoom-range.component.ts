e21aad00a49e8b9596d1acee492f26d59eeae375
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-12-09 11:59:28 -0300

    feat:319. Obtener las coordenadas centrales del mapa

--- projects/09_mapas-app/src/app/modules/mapas/pages/zoom-range/zoom-range.component.ts
+++ projects/09_mapas-app/src/app/modules/mapas/pages/zoom-range/zoom-range.component.ts
@@ -13,23 +13,34 @@ export class ZoomRangeComponent {
   public mapa: mapboxgl.Map = {} as mapboxgl.Map;
 
   public zoomLevel: number = 18;
+  public center: [number, number] = [ -58.444216881596624, -34.60446199121021 ];
 
-  ngAfterViewInit(): void {
-    // console.log(this.zoomLevel);
+  ngAfterViewInit(){
     this.mapa = new mapboxgl.Map({
       container: this.mapaWrapper.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [ -58.444216881596624, -34.60446199121021 ],
+      center: this.center,
       zoom: this.zoomLevel
     });
 
-    this.mapa.on('zoom', (): void => {
+    this.mapa.on('move', (event) => {
+      const {lng, lat } = event.target.getCenter();
+      this.center = [lng, lat];
+    });
+
+    this.mapa.on('zoom', () => {
       this.zoomLevel = this.mapa.getZoom();
     });
 
-    this.mapa.on('zoomend', (): void => {
+    this.mapa.on('zoomend', () => {
       if(this.mapa.getZoom() > 18) this.mapa.zoomTo(18);
     });
+  };
+
+  ngOnDestroy(){
+    this.mapa.off('move', () => {});
+    this.mapa.off('zoom', () => {})
+    this.mapa.off('zoomend', () => {})
   };
 
   zoom(direction: string): void {
