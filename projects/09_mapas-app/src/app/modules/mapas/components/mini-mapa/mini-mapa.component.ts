56af2dec36b0cff818233d81f779c9d5ef40d65b
Mauro Daniel Viveros <mviveros@teknosgroup.com>
2023-02-19 18:39:15 -0300

    feat: :sparkles: 328. Componente MiniMapa

--- projects/09_mapas-app/src/app/modules/mapas/components/mini-mapa/mini-mapa.component.ts
+++ projects/09_mapas-app/src/app/modules/mapas/components/mini-mapa/mini-mapa.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-mini-mapa',
-  template: `<p>mini-mapa works!</p>`,
-  styles: []
+  template: `<div #mapa></div>`,
+  styles: [
+    `
+      div{
+        width: 100%;
+        height: 150px;
+        margin: 0px;
+      }
+    `
+  ]
 })
 export class MiniMapaComponent {
+  public mapa: mapboxgl.Map = {} as mapboxgl.Map;
 
-}
+  @Input() public lngLat: [number, number] = [0, 0];
+  @ViewChild("mapa") public divMapa: ElementRef = {} as ElementRef;
+
+  ngAfterViewInit(): void{
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.lngLat,
+      interactive: false,
+      zoom: 15
+    });
+
+    new mapboxgl.Marker()
+      .setLngLat(this.lngLat)
+      .addTo(this.mapa);
+  };
+};
