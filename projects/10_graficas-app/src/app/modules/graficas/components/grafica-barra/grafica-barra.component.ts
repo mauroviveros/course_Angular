952b22410cb5ebbce07683cdf93a5ed8e71b84e9
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-03-06 09:17:45 -0300

    feat: 340. Añadir flexibilidad a nuestro componente personalizado

--- projects/10_graficas-app/src/app/modules/graficas/components/grafica-barra/grafica-barra.component.ts
+++ projects/10_graficas-app/src/app/modules/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
-import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { Component, ViewChild, Input } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartDataset, ChartEvent, ChartType, LabelItem } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
@@ -8,41 +8,30 @@ import { BaseChartDirective } from 'ng2-charts';
 })
 export class GraficaBarraComponent {
   @ViewChild(BaseChartDirective) chart: BaseChartDirective = {} as BaseChartDirective;
-
-  public barChartOptions: ChartConfiguration['options'] = {
-    responsive: true,
-    scales: { x: {}, y: { min: 10 } },
-    plugins: {
-      legend: { display: true, }
-    }
-  };
-
+  @Input() public barChartData: ChartDataset[] = [];
+  @Input() public barChartLabels: string[] = [];
+  @Input() public horizontal: boolean = false;
   public barChartType: ChartType = 'bar';
-  public barChartPlugins = [];
+  public barChartOptions: ChartConfiguration['options'] = { responsive: true };
 
-  public barChartData: ChartData<'bar'> = {
-    labels: [ '2023', '2024', '2025', '2026', '2027', '2028', '2029' ],
-    datasets: [
-      { data: Array(7).fill(0), label: 'Series A', backgroundColor: "#740AA6"},
-      { data: Array(7).fill(0), label: 'Series B', backgroundColor: "#241199" },
-      { data: Array(7).fill(0), label: 'Series C', backgroundColor: "#0A48A6" }
-    ]
-  };
+  ngOnInit(){
+    if(this.horizontal && this.barChartOptions){
+      this.barChartOptions.indexAxis = "y";
+    }
+  }
 
   ngAfterViewInit(){
     this.randomize();
   }
 
-  public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void { console.log(event, active); }
-
-  public chartHovered({ event, active }: { event?: ChartEvent, active?: {}[] }): void { console.log(event, active); }
-
   public randomize(): void {
-    for(let i = 0; i < this.barChartData.datasets.length; i++){
-      this.barChartData.datasets[i].data = this.barChartData.datasets[i].data.map(()=> {
+    for(let i = 0; i < this.barChartData.length; i++){
+      this.barChartData[i].data = this.barChartData[i].data.map(()=> {
         return Math.round(Math.random() * 100);
       });
     };
+
+    console.log(this.barChartData);
 
     this.chart?.update();
   };
