53c69d41d9e369b18fad1e4d9a4bb9cfbd163dbc
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-03-06 13:21:01 -0300

    feat: 341. Gráfica de Dona

--- projects/10_graficas-app/src/app/modules/graficas/pages/dona/dona.component.ts
+++ projects/10_graficas-app/src/app/modules/graficas/pages/dona/dona.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
+import { ChartData, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-dona',
-  template: `
-    <p>
-      dona works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: "./dona.component.html",
+  styles: [`
+    .row{ justify-content: center; }
+    .col{ max-width: 50% }
+  `]
 })
 export class DonaComponent {
-
+  public doughnutChartType: ChartType = 'doughnut';
+  public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [ { data: [ 350, 450, 100 ] }]
+  };
 }
