94d2864bac35fc663bcb8d7db763b884538bd6e7
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-03-17 22:13:09 -0300

    feat(course): :white_check_mark: 352. Directive Input - Cambiar el color del host

--- projects/11_directivas-app/src/app/shared/directives/err-msg.directive.ts
+++ projects/11_directivas-app/src/app/shared/directives/err-msg.directive.ts
@@ -1,15 +1,31 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, Type } from '@angular/core';
 
+type TypeError = "error" | "info" | "warning";
+
+enum TypeErrorColor {
+  error = "red",
+  info = "blue",
+  warning = "yellow"
+};
 @Directive({
   selector: '[error-msg]'
 })
 export class ErrMsgDirective {
+  public htmlElement: ElementRef<HTMLElement> = {} as ElementRef<HTMLElement>;
+  @Input() public type: TypeError = "error";
 
-  constructor() {
-    console.log("constructor directive error-msg");
+  constructor(
+    private _elementRef: ElementRef<HTMLElement>
+  ) {
+    this.htmlElement = this._elementRef;
   }
 
   ngOnInit(): void{
     console.log("ngOnInit directive error-msg");
+    this.setColor();
+  }
+
+  setColor(): void{
+    this.htmlElement.nativeElement.style.color = TypeErrorColor[this.type];
   }
 }
