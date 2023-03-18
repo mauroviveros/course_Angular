76d25bb6030198c959fb10eb4655388f660d5f68
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-03-18 00:38:54 -0300

    feat(course): :white_check_mark: 356. Resolución del problema

--- projects/11_directivas-app/src/app/shared/directives/err-msg.directive.ts
+++ projects/11_directivas-app/src/app/shared/directives/err-msg.directive.ts
@@ -4,16 +4,25 @@ import { Directive, ElementRef, Input } from '@angular/core';
   selector: '[error-msg]'
 })
 export class ErrMsgDirective {
+  private _default = {
+    color: "red",
+    text: "Campo invalido"
+  }
   @Input() public set color(valor: string){
-    this._elementRef.nativeElement.style.color = valor || "red";
+    this._elementRef.nativeElement.style.color = valor || this._default.color;
   }
   @Input("error-msg") public set message(valor: string){
-    this._elementRef.nativeElement.innerHTML = valor;
+    this._elementRef.nativeElement.innerHTML = valor || this._default.text;
   }
 
   constructor(
     private _elementRef: ElementRef<HTMLElement>
   ) {
     this._elementRef.nativeElement.classList.add("form-text");
+  }
+  ngOnInit(): void{
+    this._elementRef.nativeElement.style.color = this._default.color;
+    this._elementRef.nativeElement.innerHTML = this._default.text;
+
   }
 }
