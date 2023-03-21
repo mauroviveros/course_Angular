409a64ab5e703d81550deeead139295f4475e437
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-03-21 15:05:58 -0300

    feat(course): :white_check_mark: 372. Validar campos obligatorios

--- backend/12_auth-app/controllers/auth.js
+++ backend/12_auth-app/controllers/auth.js
@@ -1,11 +1,19 @@
 const { response, request } = require("express");
+const { validationResult } = require("express-validator");
 
 const register = (req = request, res = response) => {
     return res.json({ ok: true, message: "Crear Usuario" });
 };
 
 const login = (req = request, res = response) => {
-    return res.json({ ok: true, message: "Login Usuario" });
+    const errors = validationResult(req);
+    try {
+        if(!errors.isEmpty()) throw errors;
+        return res.json({ ok: true, message: "Login Usuario" });
+    } catch (error) {
+        return res.status(400).json({ ok: false, errors: errors.mapped() });
+    };
+
 };
 
 const getUser = (req = request, res = response) => {
--- backend/12_auth-app/routes/auth.js
+++ backend/12_auth-app/routes/auth.js
@@ -1,11 +1,15 @@
 const { Router } = require("express");
+const { check } = require("express-validator");
 const { getUser, register, login } = require("../controllers/auth");
 
 const router = Router();
 
 router.get("/user/:_id", getUser);
 router.post("/register", register);
-router.post("/login", login);
+router.post("/login", [
+    check("email", "El email es obligatorio").isEmail(),
+    check("password", "La contraseña es obligatoria").isLength({ min: 6 })
+], login);
 
 
 module.exports = router;
