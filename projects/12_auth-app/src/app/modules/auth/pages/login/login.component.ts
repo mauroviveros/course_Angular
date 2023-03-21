bc1615efc175b8b343c369d8030fe4042c800035
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-03-21 17:52:46 -0300

    feat(course): :white_check_mark: 391. Diseño de la pantalla de Registro y Login

--- projects/12_auth-app/src/app/modules/auth/pages/login/login.component.ts
+++ projects/12_auth-app/src/app/modules/auth/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'auth-login',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  public form: FormGroup = this._formBuilder.group({
+    email: ["user1@test.com", [Validators.required, Validators.email]],
+    password: ["123456", [Validators.required, Validators.minLength(6)]]
+  })
+
+  constructor(
+    private _formBuilder: FormBuilder
+  ){};
+
+  public login(): void{
+    console.log(this.form.value);
+    console.log(this.form.valid);
+  }
 }
--- projects/12_auth-app/src/app/modules/auth/pages/register/register.component.ts
+++ projects/12_auth-app/src/app/modules/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'auth-register',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  public form: FormGroup = this._formBuilder.group({
+    name: ["", [Validators.required]],
+    email: ["", [Validators.required, Validators.email]],
+    password: [null, [Validators.required, Validators.minLength(6)]]
+  })
 
+  constructor(
+    private _formBuilder: FormBuilder
+  ){};
+
+  public register(): void{
+    console.log(this.form.value);
+    console.log(this.form.valid);
+  }
 }
