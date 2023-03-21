66550907b012409b6727c4109fc9f3f7edb2e4c3
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-03-21 15:13:20 -0300

    feat(course): :white_check_mark: 373. Tarea: Validar campos

--- backend/12_auth-app/controllers/auth.js
+++ backend/12_auth-app/controllers/auth.js
@@ -2,18 +2,25 @@ const { response, request } = require("express");
 const { validationResult } = require("express-validator");
 
 const register = (req = request, res = response) => {
-    return res.json({ ok: true, message: "Crear Usuario" });
+    const errors = validationResult(req);
+
+    try {
+        if(!errors.isEmpty()) throw errors;
+        return res.json({ ok: true, message: "Crear Usuario" });
+    } catch (error) {
+        return res.status(400).json({ ok: false, errors: errors.mapped() });
+    };
 };
 
 const login = (req = request, res = response) => {
     const errors = validationResult(req);
+
     try {
         if(!errors.isEmpty()) throw errors;
         return res.json({ ok: true, message: "Login Usuario" });
     } catch (error) {
         return res.status(400).json({ ok: false, errors: errors.mapped() });
     };
-
 };
 
 const getUser = (req = request, res = response) => {
--- backend/12_auth-app/routes/auth.js
+++ backend/12_auth-app/routes/auth.js
@@ -5,7 +5,11 @@ const { getUser, register, login } = require("../controllers/auth");
 const router = Router();
 
 router.get("/user/:_id", getUser);
-router.post("/register", register);
+router.post("/register", [
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+    check("email", "El email es obligatorio").isEmail(),
+    check("password", "La contraseña es obligatoria").isLength({ min: 6 })
+], register);
 router.post("/login", [
     check("email", "El email es obligatorio").isEmail(),
     check("password", "La contraseña es obligatoria").isLength({ min: 6 })
