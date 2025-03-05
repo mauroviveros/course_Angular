5ea8e6829927f7fd338f4ab7d0c37d0d42a464f3
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-03-04 22:00:58 -0300

    feat: :sparkles: compare legacy with signal

--- bases/src/app/pages/counter/counter.component.ts
+++ bases/src/app/pages/counter/counter.component.ts
@@ -1,22 +1,32 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   imports: [],
   templateUrl: './counter.component.html',
-  styles: [`button { padding: 5px; margin: 5px 10px; width: 75px; }`]
+  styles: [`button { padding: 5px; margin: 5px 10px; width: 75px; }`],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CounterComponent {
-  public counter: number = 0;
-public counterSignal = signal<number>(0);
+  public counterLegacy: number = 0;
+  public counter = signal<number>(0);
+
+
+  constructor(){
+    // setInterval(() => {
+    //   console.log('tick');
+    //   this.increment(1);
+    // }, 2000);
+  }
+
 
   increment(value: number): void {
-    this.counter += value;
-    this.counterSignal.update(counter => counter + value);
+    this.counterLegacy += value;
+    this.counter.update(counter => counter + value);
   }
 
   reset(): void {
-    this.counter = 0;
-    this.counterSignal.set(0);
+    this.counterLegacy = 0;
+    this.counter.set(0);
   }
 }
