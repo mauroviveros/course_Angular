d3590a3c7f49388b9b3427958b85c71fd5e944ba
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-03-20 23:09:40 -0300

    feat: :sparkles: add homework solution

---
+++ bases/src/app/pages/hero-page/hero-page.component.ts
@@ -0,0 +1,32 @@
+import { TitleCasePipe } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-page',
+  imports: [TitleCasePipe],
+  templateUrl: './hero-page.component.html',
+  styles: ``
+})
+export class HeroPageComponent {
+  readonly name = signal<string>('Ironman');
+  readonly age = signal<number>(45);
+
+  readonly description = computed(() => {
+    return `${this.name()} is ${this.age()} years old`;
+  })
+
+
+  changeHero(): void {
+    this.name.set('Spiderman');
+    this.age.set(22);
+  }
+
+  changeAge(): void {
+    this.age.set(60);
+  }
+
+  resetForm(): void {
+    this.name.set('Ironman');
+    this.age.set(45);
+  }
+}
