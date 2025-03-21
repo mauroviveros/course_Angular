7416f5333b6f8a0dd1e6cfddf45bd6fadfae5c0e
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-03-21 12:16:41 -0300

    feat: :sparkles: add navbar

--- bases/src/app/app.component.ts
+++ bases/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   template: `
+    <app-navbar></app-navbar>
     <section class="mx-5 mt-2">
       <router-outlet />
     </section>
--- bases/src/app/app.routes.ts
+++ bases/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { CounterComponent } from './pages/counter/counter.component';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
+import { DragonballPageComponent } from './pages/dragonball-page/dragonball-page.component';
 
 export const routes: Routes = [
-  { path: '', component: CounterComponent },
-  { path: 'hero', component: HeroPageComponent }
+  { path: 'counter', component: CounterComponent },
+  { path: 'hero', component: HeroPageComponent },
+  // { path: 'dragonball', component: DragonballPageComponent },
+  { path: '**', redirectTo: '' }
 ];
